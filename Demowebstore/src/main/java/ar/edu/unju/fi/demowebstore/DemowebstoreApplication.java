76979ceb45185219d669be7390d50533de321199
Marisel A. Yapura <35896699@fi.unju.edu.ar>
2021-11-15 00:49:59 -0300

    Responsable: Marisel A Yapura Tareas realizadas: Creación del proyecto

---
+++ Demowebstore/src/main/java/ar/edu/unju/fi/demowebstore/DemowebstoreApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.demowebstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowebstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowebstoreApplication.class, args);
+	}
+
+}
